bb60d7eaddb96241e8e9c7509d11c50304f61819
Paraskevas <93684742+paraskevas2021@users.noreply.github.com>
2022-11-05 14:14:12 +0200

    Add files via upload

---
+++ askisi2.py
@@ -0,0 +1,16 @@
+# Giannakopoulos Paraskevas 321/2020040
+# Konstantaras Ioannis 321/2020105
+# Anagnostopoulos Georgios 321/2020010
+import csv
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#Diavazei to csv me onoma askisi1 apo to 1o erwtima
+dataframe = pd.read_csv('askisi1.csv')
+
+#Orizoume tis metavlites pou simmetexoun sto diagramma 
+dataframe.plot(kind='scatter', x = 'Miles_Per_Galon', y = 'Miles')
+
+#Emfanise to dataframe kai to diagramma 
+print(dataframe.to_string)
+plt.show()
